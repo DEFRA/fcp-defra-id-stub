8be7440219efda487e9e514b9002ef715df0992b
John Watson <john.watson1@defra.gov.uk>
2025-08-31 19:49:23 +0100

    Update index page

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,6 +1,6 @@
 import Inert from '@hapi/inert'
 import { health } from '../routes/health.js'
-import { start } from '../routes/start.js'
+import { index } from '../routes/index.js'
 import { openId } from '../routes/open-id.js'
 import { auth } from '../routes/auth.js'
 import { serveStaticFiles } from '../common/helpers/serve-static-files.js'
@@ -11,7 +11,7 @@ export const router = {
     async register (server) {
       await server.register([Inert])
       await server.route(health)
-      await server.route(start)
+      await server.route(index)
       await server.route(openId)
       await server.route(auth)
       await server.register([serveStaticFiles])
