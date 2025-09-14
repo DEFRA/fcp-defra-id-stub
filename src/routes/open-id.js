bc7962a99349a7a4347f2bdc29ed6fd3a4276434
John Watson <john.watson1@defra.gov.uk>
2025-09-14 15:07:38 +0100

    Add OIDC validation

--- src/routes/open-id.js
+++ src/routes/open-id.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import Boom from '@hapi/boom'
 import { getWellKnown } from '../open-id/get-well-known.js'
 import { getTokens, getPublicKeys, endSession } from '../auth/token.js'
 
@@ -37,13 +38,29 @@ const authorization = {
   }
 }
 
+const tokenSchema = Joi.object({
+  grant_type: Joi.string().valid('authorization_code', 'refresh_token').required(),
+  code: Joi.string(),
+  redirect_uri: Joi.string().uri().required(),
+  client_id: Joi.string().required(),
+  client_secret: Joi.string().required(),
+  scope: Joi.string(),
+  refresh_token: Joi.string()
+})
+
 const token = {
   method: 'POST',
   path: '/dcidmtest.onmicrosoft.com/b2c_1a_cui_cpdev_signupsigninsfi/oauth2/v2.0/token',
   handler: function (request, h) {
-    console.log('Token request', request.payload, request.query)
-    const { code: accessCode } = request.payload
-    const { grant_type: grantType, refresh_token: refreshToken } = request.query
+    const params = { ...request.query, ...request.payload }
+
+    const { error } = tokenSchema.validate(params)
+
+    if (error) {
+      throw Boom.badRequest(`${error.message}`)
+    }
+
+    const { code: accessCode, grant_type: grantType, refresh_token: refreshToken } = params
 
     const tokens = getTokens(accessCode, grantType, refreshToken)
 
@@ -58,8 +75,19 @@ const token = {
 const signOut = {
   method: 'GET',
   path: '/idphub/b2c/b2c_1a_cui_cpdev_signupsigninsfi/signout',
+  options: {
+    validate: {
+      query: {
+        post_logout_redirect_uri: Joi.string().uri().required(),
+        id_token_hint: Joi.string().required(),
+        state: Joi.string()
+      },
+      failAction: async (_request, h, error) => h.view('errors/400', {
+        message: error.message
+      }).takeover()
+    }
+  },
   handler: function (request, h) {
-    console.log('Sign out request', request.query)
     const {
       post_logout_redirect_uri: redirectUri,
       id_token_hint: accessToken,
@@ -70,7 +98,9 @@ const signOut = {
 
     request.yar.reset()
 
-    return h.redirect(`${redirectUri}?state=${state}`)
+    const stateResponse = state ? `?state=${state}` : ''
+
+    return h.redirect(`${redirectUri}${stateResponse}`)
   }
 }
 
