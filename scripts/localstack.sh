60329da85e858c0a457e9643bb79bec72c544e07
John Watson <john.watson1@defra.gov.uk>
2026-03-31 13:17:58 +0100

    Replace Localstack

--- scripts/localstack.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "configuring s3"
-echo "==================="
-
-awslocal s3api create-bucket \
-  --bucket fcp-defra-id-stub-data \
-  --create-bucket-configuration LocationConstraint=eu-west-2
